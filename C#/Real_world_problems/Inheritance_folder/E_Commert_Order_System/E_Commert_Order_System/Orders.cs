c0d56e2ae7ccd6cb450f4f3e65f583e6b41a7279
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-14 13:04:21 +0530

    Completed writing the inheritance code for business E-Commers Order system

---
+++ C#/Real_world_problems/Inheritance_folder/E_Commert_Order_System/E_Commert_Order_System/Orders.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commers_Order_System
+{
+    class Order
+    {
+        public int OrderId { get; private set; }
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; private set; }
+
+        public Order(int id, string name, DateTime date, decimal amount)
+        {
+            OrderId = id;
+            CustomerName = name;
+            OrderDate = date;
+            if (amount > 0)
+            {
+                TotalAmount = amount;
+            }
+            else
+            {
+                throw new ArgumentException("Order amount shouldn't be negative");
+            }
+        }
+
+        public void DisplayOrderDetails()
+        {
+            Console.WriteLine("Order ID: " + OrderId);
+            Console.WriteLine("Customer Name: " + CustomerName);
+            Console.WriteLine("Order Date: " + OrderDate);
+            Console.WriteLine("Total Amount: " + TotalAmount);
+        }
+
+    }
+    class OnlineOrder: Order
+    {
+        public string DeliveryAddress { get; set; }
+        public decimal ShippingFee { get; set; }
+
+        public OnlineOrder(int id, string name, DateTime date, decimal amount, string address, decimal shippingfee):base(id, name, date, amount)
+        {
+            DeliveryAddress = address;
+            ShippingFee = shippingfee;
+        }
+
+        public void CalculateFinalAmount()
+        {
+            Console.WriteLine("Order amount: "+ TotalAmount);
+            Console.WriteLine("Shipping Fee: " + ShippingFee);
+            decimal FinalFee = TotalAmount + ShippingFee;
+            Console.WriteLine("Final Amount: " + FinalFee);
+        }
+    }
+    class StorePickupOrder: Order
+    {
+        public string StoreLocation { get; set; }
+        public string PickupPersonName { get; set; }
+        public StorePickupOrder(int id, string name, DateTime date, decimal amount, string location, string pickupperson) : base(id, name, date, amount)
+        {
+            StoreLocation = location;
+            PickupPersonName = pickupperson;
+        }
+
+        public void PrepareForPickup()
+        {
+            Console.WriteLine("Order is ready for pickup at Bangalore Store.");
+            Console.WriteLine($"Pickup person: {PickupPersonName}");
+        }
+    }
+    class CorporateOrder:Order
+    {
+        public string CompanyName { get; set; }
+        public int NumberOfEmployees { get; set; }
+
+        public CorporateOrder(int id, string name, DateTime date, decimal amount, string companyname, int noofemployee) : base(id, name, date, amount)
+        {
+            CompanyName = companyname;
+            NumberOfEmployees = noofemployee;
+        }
+
+        public void CalculateDiscount()
+        {
+            decimal Discount = TotalAmount * 5 / 100;
+            Console.WriteLine("Order amount: " + TotalAmount);
+            Console.WriteLine("Discount : " + Discount);
+            decimal FinalAmount = TotalAmount - Discount;
+            Console.WriteLine("Final Amount: " + FinalAmount);
+        }
+    }
+}
---
+++ C#/Real_world_problems/Inheritance_folder/E_Commert_Order_System/E_Commert_Order_System/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commers_Order_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Online Order===========");
+            OnlineOrder oo = new OnlineOrder(12, "Revanth", new DateTime(2005, 10, 05), 7500000, "Peddarikunta", 50);
+            oo.DisplayOrderDetails();
+            oo.CalculateFinalAmount();
+
+            Console.WriteLine("==========Store Pickup Order===========");
+            StorePickupOrder sp = new StorePickupOrder(45, "Arjuna", new DateTime(2007, 10, 17), -500000, "Peddarikunta", "Bharath");
+            sp.DisplayOrderDetails();
+            sp.PrepareForPickup();
+
+            Console.WriteLine("==========Corporate Order===========");
+            CorporateOrder cpr = new CorporateOrder(5, "Sun", new DateTime(0001, 01, 01), 2000000, "Amazon", 500001);
+            cpr.DisplayOrderDetails();
+            cpr.CalculateDiscount();
+
+            Console.ReadLine();
+        }
+    }
+}
