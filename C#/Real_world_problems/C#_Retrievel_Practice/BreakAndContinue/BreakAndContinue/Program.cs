d59d16debe9b204f22f3a9f28e004d3e7c924993
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-18 21:23:44 +0530

    feat: demonstrate break and continue control flow

---
+++ C#/Real_world_problems/C#_Retrievel_Practice/BreakAndContinue/BreakAndContinue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<=10; i++)
+            {
+                if (i == 5)
+                {
+                    continue;
+                    
+                }
+                else if (i == 8)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
