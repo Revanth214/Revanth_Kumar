778c76adf542475fdcdb636312ee45bdaec9d370
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-18 20:38:49 +0530

    Add employee data types and variable input program

---
+++ C#/Real_world_problems/C#_Retrievel_Practice/Data_Types_and_Variables/Data_Types_and_Variables/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types_and_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee Name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter Employee Age: ");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Salary: ");
+            decimal Salary =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Is employee still working : ");
+            string Is_Curr_Working = Console.ReadLine();
+            Console.WriteLine("Enter Employee Initial: ");
+            string First_initial = Console.ReadLine();
+
+            Console.WriteLine("Employee Name: " + Name);
+            Console.WriteLine("Employee Age: " + Age);
+            Console.WriteLine("Employee Salary: " + Salary);
+            Console.WriteLine("Employee Is still working: " + Is_Curr_Working);
+            Console.WriteLine("Employee first intial: " + First_initial);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
