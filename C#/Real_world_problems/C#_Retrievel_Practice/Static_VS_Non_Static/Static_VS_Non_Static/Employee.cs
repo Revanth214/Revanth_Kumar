8209a095107d67cbd5f13a7693acf3daf65b45b4
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-19 09:22:24 +0530

    feat: demonstrate static and non-static methods

---
+++ C#/Real_world_problems/C#_Retrievel_Practice/Static_VS_Non_Static/Static_VS_Non_Static/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_VS_Non_Static
+{
+    class Employee
+    {
+        public string EmployeeName;
+        public decimal Salary;
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Employee Name: " + EmployeeName);
+            Console.WriteLine("Employee Salary: " + Salary);
+        }
+
+        public static decimal CalculateTax(decimal salary)
+        {
+            return salary * 10 / 100;
+        }
+    }
+}
---
+++ C#/Real_world_problems/C#_Retrievel_Practice/Static_VS_Non_Static/Static_VS_Non_Static/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_VS_Non_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ey = new Employee();
+            ey.EmployeeName = "Revanth";
+            ey.Salary = 1000000;
+            ey.DisplayDetails();
+            decimal tax=Employee.CalculateTax(200000);
+            Console.WriteLine("Tax: " + tax);
+
+            Console.ReadLine();
+        }
+    }
+}
