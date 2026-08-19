b81735cd276b6bc9b44dad0ffdadb78f3a19ae52
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-19 10:05:45 +0530

    feat: implement employee record duplication through copy construction

---
+++ C#/Real_world_problems/C#_Retrievel_Practice/Copy_Constructor/Copy_Constructor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Copy_Constructor
+{
+    class Employee
+    {
+        public string EmployeeName;
+        public decimal Salary;
+        public string Department;
+
+        public Employee(string name, decimal salary, string department)
+        {
+            EmployeeName = name;
+            Salary = salary;
+            Department = department;
+        }
+        public Employee(Employee cpy)
+        {
+            EmployeeName = cpy.EmployeeName;
+            Salary = cpy.Salary;
+            Department = cpy.Department;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Employee Name: " + EmployeeName);
+            Console.WriteLine("Employee Salary: " + Salary);
+            Console.WriteLine("Employee Department: " + Department);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ee = new Employee("Bharath", 5000000, "Entertinement");
+            Employee ee2 = new Employee(ee);
+            ee2.Salary = 20000;
+
+            Console.WriteLine("Original:");
+            ee.Display();
+
+            Console.WriteLine("Copied:");
+            ee2.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
