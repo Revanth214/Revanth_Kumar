c489c1f48ffae9e39b8d8f82cdf0c31c5ab869f7
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-19 14:33:53 +0530

    feat: process and transform employee names with string operations

---
+++ C#/Real_world_problems/C#_Retrievel_Practice/Strings/Strings/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strng = "   Revanth Kumar   ";
+
+            Console.WriteLine("Original string: " + strng);
+
+            string cleanedString = strng.Trim();
+
+            Console.WriteLine("After Trim: " + cleanedString);
+            Console.WriteLine("Convert to Upper Case: " + cleanedString.ToUpper());
+            Console.WriteLine("Convert to Lower Case: " + cleanedString.ToLower());
+
+            Console.WriteLine(
+                "Length of the cleaned string: " + cleanedString.Length);
+
+            Console.WriteLine(
+                "Whether string contains 'a': " +
+                cleanedString.ToLower().Contains('a'));
+
+            Console.WriteLine(
+                "Replace ' ' with '-': " +
+                cleanedString.Replace(' ', '-'));
+
+            Console.WriteLine(
+                "First 5 characters: " +
+                cleanedString.Substring(0, 5));
+
+            Console.WriteLine("Cleaned string: " + cleanedString);
+
+            Console.ReadLine();
+        }
+    }
+}
