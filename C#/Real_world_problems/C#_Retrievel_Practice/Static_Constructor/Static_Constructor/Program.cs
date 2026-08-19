9f922c8e32855b00bc40e08b1ed0c92e3ad47fbd
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-19 09:47:58 +0530

    feat: initialize company data with static constructor

---
+++ C#/Real_world_problems/C#_Retrievel_Practice/Static_Constructor/Static_Constructor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Constructor
+{
+    class Company
+    {
+        static string CompanyName;
+        static int EmployeeCount;
+
+        static Company()
+        {
+            Console.WriteLine("Static constructor executed.");
+            CompanyName = "TechCorp";
+            EmployeeCount = 500;
+        }
+
+        public void DisplayCompany()
+        {
+            Console.WriteLine("Company Name: " + CompanyName);
+            Console.WriteLine("Employee Count: " + EmployeeCount);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company cm = new Company();
+            cm.DisplayCompany();
+            Console.ReadLine();
+        }
+    }
+}
