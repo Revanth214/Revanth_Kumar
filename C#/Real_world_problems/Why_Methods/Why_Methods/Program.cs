4bd4722b5226e272cdf33952d50552f47b50f057
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-22 12:31:08 +0530

    Completed revision of methods

---
+++ C#/Real_world_problems/Why_Methods/Why_Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Why_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> my_list = new List<int>();
+            my_list.Add(5);
+            my_list.Add(12);
+            my_list.Add(10);
+            void Show()
+            {
+                foreach (var item in my_list)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            Show();
+            my_list.Add(20);
+
+            Console.WriteLine("After Adding another element");
+            Show();
+            Console.ReadLine();
+        }
+    }
+}
