fcfd1d5192f7f5abc8b956e428ba22f05c6ee6b8
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-23 14:14:00 +0530

    feat: implement palindrome checker using two-pointer technique

---
+++ C#/Real_world_problems/Collections_Practice/Dict_Collection/Two_Pointer_Technique/Two_Pointer_Technique/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Pointer_Technique
+{
+    class Program
+    {
+        static bool IsPalindrome(string text)
+        {
+            int left = 0;
+            int right = text.Length - 1;
+            while (left < right)
+            {
+                if (text[left] != text[right])
+                {
+                return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a text: ");
+            string text = Console.ReadLine();
+            if (IsPalindrome(text))
+            {
+                Console.WriteLine("Polindrome");
+            }
+            else
+            {
+                Console.WriteLine("Not a Polindrome");
+            }    
+           
+
+
+            Console.ReadLine();
+        }
+    }
+}
