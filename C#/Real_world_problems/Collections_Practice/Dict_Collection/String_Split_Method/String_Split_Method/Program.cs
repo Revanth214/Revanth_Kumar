687d34d2d7441e60c379d3e5a66a1870bf8ef5e1
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-23 14:32:21 +0530

    feat: add longest word finder using Split and foreach

---
+++ C#/Real_world_problems/Collections_Practice/Dict_Collection/String_Split_Method/String_Split_Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Split_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            string[] data = text.Split(' ');
+            string Largest_text = "";
+            foreach(var txt in data)
+            {
+                if (Largest_text.Length < txt.Length)
+                {
+                    Largest_text = txt;
+                }
+            }
+            Console.WriteLine("Largest text in sentence: " + Largest_text);
+
+            Console.ReadLine();
+        }
+    }
+}
