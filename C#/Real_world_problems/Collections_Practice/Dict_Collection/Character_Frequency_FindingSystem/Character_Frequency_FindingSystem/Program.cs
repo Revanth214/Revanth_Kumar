6bdd28b02c19a96b90c9d950b1c05354e1966e87
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-23 14:59:25 +0530

    feat: add character frequency counter using dictionary

---
+++ C#/Real_world_problems/Collections_Practice/Dict_Collection/Character_Frequency_FindingSystem/Character_Frequency_FindingSystem/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Character_Frequency_FindingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+            string CleanedText = text.ToLower().Replace(" ", "");
+            Dictionary<char, int> data = new Dictionary<char, int>();
+            foreach(char ch in CleanedText)
+            {
+                if (data.ContainsKey(ch))
+                {
+                    data[ch]++;
+                }
+                else
+                {
+                    data[ch] = 1;
+                }
+            }
+            foreach(var i in data)
+            {
+                Console.WriteLine($"{i.Key} : {i.Value}");
+                
+            }
+            Console.WriteLine("Total Characters: " + CleanedText.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
