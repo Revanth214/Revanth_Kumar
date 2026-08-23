6b8109ca8ecc434705738b3d8db5657f6f1eca29
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-23 13:41:57 +0530

    Completed finding duplicates using Dictionary

---
+++ C#/Real_world_problems/Collections_Practice/Dict_Collection/Dict_Count_program/Dict_Count_program/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dict_Count_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 20, 10, 30, 20 };
+            Dictionary<int, int> elements = new Dictionary<int, int>();
+
+            foreach(int number in arr)
+            {
+                if (elements.ContainsKey(number))
+                {
+                    elements[number]++;
+                }
+                else
+                {
+                    elements[number] = 1;
+                }
+            }
+
+            foreach(KeyValuePair<int, int> variable in elements)
+            {
+                Console.WriteLine($"{variable.Key} : {variable.Value}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
