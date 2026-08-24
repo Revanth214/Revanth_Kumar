edf4d6030f4021ea4a7be5b0ea95aa6360d3a5e9
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-24 21:35:18 +0530

    feat(ado.net): implement first ADO.NET database connection and operations

---
+++ C#/Real_world_problems/ADO_NET/First_ADO_Code/First_ADO_Code/EmployeeModel.cs
@@ -0,0 +1,11 @@
+﻿namespace First_ADO_Code
+{
+    class EmployeeModel
+    {
+        public int EMP_ID { get; set; }
+
+        public string EMP_Name { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
---
+++ C#/Real_world_problems/ADO_NET/First_ADO_Code/First_ADO_Code/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace First_ADO_Code
+{
+    class EmployeeDetails
+    {
+        SqlConnection connection = new SqlConnection(
+            "Server=LAPTOP-RBLEO; Database=SQLMasteryLab; Integrated Security=true"
+        );
+
+        public List<EmployeeModel> GetEmpDetails()
+        {
+            SqlCommand cmd = new SqlCommand(
+                "SELECT * FROM Employee",
+                connection
+            );
+
+            SqlDataAdapter ada = new SqlDataAdapter(cmd);
+
+            DataSet ds = new DataSet();
+
+            connection.Open();
+
+            ada.Fill(ds);
+
+            connection.Close();
+
+            List<EmployeeModel> lst = new List<EmployeeModel>();
+
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                EmployeeModel model = new EmployeeModel();
+
+                model.EMP_ID = Convert.ToInt32(dr["EMP_ID"]);
+                model.EMP_Name = dr["EMP_Name"].ToString();
+                model.Salary = Convert.ToDecimal(dr["Salary"]);
+
+                lst.Add(model);
+            }
+
+            return lst;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeDetails model = new EmployeeDetails();
+
+            List<EmployeeModel> employees = model.GetEmpDetails();
+
+            foreach (EmployeeModel employee in employees)
+            {
+                Console.WriteLine("Employee ID: " + employee.EMP_ID);
+                Console.WriteLine("Employee Name: " + employee.EMP_Name);
+                Console.WriteLine("Salary: " + employee.Salary);
+                Console.WriteLine("----------------------------");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
