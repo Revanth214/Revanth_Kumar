21a4f20876e1eec3f7ce5f325e1a4a9c084f3c8f
C Revanth kumar <cgangadhar214@gmail.com>
2026-08-29 09:12:44 +0530

    Connected my database data with application with c# ADO

---
+++ C#/ADO.NET/ThirdADO/ThirdADO/DepartmentDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ThirdADO
+{
+    class DepartmentDetails
+    {
+        SqlConnection connection = new SqlConnection("Server=LAPTOP-RBLEO; Database=SQLMasteryLab; Integrated Security=true");
+        
+        public List<DepartmentModel> GetDepartmentDetails()
+        {
+            SqlCommand cmd = new SqlCommand("Select * from department", connection);
+            SqlDataAdapter adp = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            connection.Open();
+            adp.Fill(ds);
+            connection.Close();
+            List<DepartmentModel> lst = new List<DepartmentModel>();
+            foreach(DataRow data in ds.Tables[0].Rows)
+            {
+                DepartmentModel model = new DepartmentModel();
+                model.Dept_Id = Convert.ToInt32(data["DEPT_ID"]);
+                model.Dept_Name = data["DEPT_NAME"].ToString();
+                model.Location = data["LOCATION"].ToString();
+
+                lst.Add(model);
+            }
+            return lst;
+        }
+    }
+}
---
+++ C#/ADO.NET/ThirdADO/ThirdADO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DepartmentDetails dept_obj = new DepartmentDetails();
+            List<DepartmentModel> model = dept_obj.GetDepartmentDetails();
+            foreach(DepartmentModel dept in model)
+            {
+                Console.WriteLine(dept.Dept_Id);
+                Console.WriteLine(dept.Dept_Name);
+                Console.WriteLine(dept.Location);
+                Console.WriteLine("=====================");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
